2265d9d7c1b560b382435dd2a1120e5e6e94d7de
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-05-30 23:54:53 +0900

    530

--- printfscanf.c
+++ printfscanf.c
@@ -21,6 +21,6 @@ int main(void)
 	// ¿¬»ê 
 	int add = 3 + 7; // 10 
 	//printf("3+7=%d\n", add);
-	printf(" % d + % d = % d\n", 3, 7, 3 + 7);
+	printf(" % d + % d = % d\n", 30, 79, 30 * 79);
 	return 0;
 }
