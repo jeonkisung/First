2ec2d3202facdca71ff3c61e3353488c3465464a
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-05-30 23:53:39 +0900

    5/30

--- printfscanf.c
+++ printfscanf.c
@@ -13,9 +13,14 @@ int main(void)
 	double d = 4.428;
 	printf("%.2lf\n", d);*/
 
-	const int YEAR 2000; // 상수 
-	print("태어난 년도 : %d\n,YEAR");
+	//const int YEAR 2000; // 상수 
+	//print("태어난 년도 : %d\n,YEAR");
 	//YEAR = 2001;
 
+	// printf
+	// 연산 
+	int add = 3 + 7; // 10 
+	//printf("3+7=%d\n", add);
+	printf(" % d + % d = % d\n", 3, 7, 3 + 7);
 	return 0;
 }
