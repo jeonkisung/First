156f8ab30b2bf6892c789a41a17b17422d315d97
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-05-31 23:42:47 +0900

    0531

--- printfscanf.c
+++ printfscanf.c
@@ -33,6 +33,9 @@ int main(void)
 	int one, two, three;
 	printf("3개의 정수를 입력하세요 : ");
 	scanf_s("%d%d%d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번째 값 : %d\n", three);
 
 	return 0;
 }
