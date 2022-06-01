3f8df0b41cade9c8c40de89d23c9150e574e9d0d
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-06-02 00:48:05 +0900

    F_W_DW

--- loop.c
+++ loop.c
@@ -22,5 +22,47 @@ int main(void)
 	printf("a 는 %d\n", a)
 	a++;*/
 
+	/*int b = 20;
+	// b = b + 1;
+	printf("b 는 %d\n", ++b);
+	printf("b 는 %d\n", b++);
+	printf("b 는 %d\n", b);*/
+
+	int i = 1;
+	/*printf("Hello World %d\n", i++);    //1
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);*/
+
+	// 반복문 
+	// for ,while, do while
+
+	//for (선언; 조건; 증감)
+	/*for (int i = 1; i <= 10; i++)
+	{
+		printf("Hello World % d\n", i);
+
+	}*/
+
+	// while (조건) { }
+	//int i = 1;
+	//while (i <= 10)
+	//{
+	//	printf("Hello World % d\n", i);
+		//i++; 
+	//}
+
+	// dp { } while ( 조건)
+	/*int i = 1;
+	do {
+		printf("Hello World % d\n", i++);
+	} while (i <= 10);*/
+
 	return 0;
 }
