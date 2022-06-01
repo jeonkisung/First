3bc56f58bc8649c5e704660f5458102328bd31bd
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-06-02 00:30:01 +0900

    ++

--- loop.c
+++ loop.c
@@ -3,5 +3,24 @@
 int main(void)
 {
 	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+
+	// ++ »Ô»Ô ¤»
+	/*int a = 10;
+	printf("a ´Â %d\n", a)
+	a++;
+	printf("a ´Â %d\n", a)
+	a++;
+	printf("a ´Â %d\n", a)
+	a++;*/
+
 	return 0;
 }
