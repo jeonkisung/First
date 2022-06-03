c3c8fd2f53ae71451acf6203a0a6f10425fe8b34
전기성 <99243495+jeonkisung@users.noreply.github.com>
2022-06-04 01:26:18 +0900

    0604

--- loop.c
+++ loop.c
@@ -64,5 +64,58 @@ int main(void)
 		printf("Hello World % d\n", i++);
 	} while (i <= 10);*/
 
+	// 2중 반복문 
+	/*for (int i = 1; i <= 3; i++)
+	{
+		printf("첫 번째 반복문 : %d\n", i);
+
+		for (int j = 1; j <= 5; j++);
+		{
+			printf("두 번째 반복문 : %d\n", i);
+
+			
+		}
+	}*/
+
+	// 구구단 
+	// 2 x 1 = 2
+	// 2 x 2 = 4
+	// 2 x 3 = 6
+	// 2 x 4 = 8 ...
+	// ...
+	// 9 x 9 = 81
+	/*for (int i = 2; i <= 9; i++)
+	{
+		printf("%d\n단 계산\n", i);
+		{
+			printf(" %d x %d = %d\n, i ,j i*j");
+		}
+	}*/
+
+	// 피라미드 
+
+	for (int i = 0; i < 5; i++)
+	{
+		for(int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+
+	// 오른쪽 피라미드 
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++)
+		{
+			printf("S");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
